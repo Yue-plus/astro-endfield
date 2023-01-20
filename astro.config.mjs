dfd921159031ec12eb90cbeeead4f1f53968be3c
Yue-plus <Yue_plus@foxmail.com>
2023-01-20 21:08:50 +0800

    add workflows

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+// https://docs.astro.build/zh-cn/reference/configuration-reference/
+export default defineConfig({
+  site: 'https://astro.endfield.tech',
+});
