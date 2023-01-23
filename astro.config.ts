5630dd00b0e3318b73a23ea416acbeac90b4ca9f
Yue-plus <Yue_plus@foxmail.com>
2023-01-23 22:39:45 +0800

    pnpx astro add react

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 // https://docs.astro.build/zh-cn/reference/configuration-reference/
 export default defineConfig({
   site: "https://astro.endfield.tech",
+  integrations: [react()]
 });
