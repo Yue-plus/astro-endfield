4ef17df45feb317668dc81dc823c5bb94930ed2b
Yue-plus <Yue_plus@foxmail.com>
2023-01-26 22:58:03 +0800

    update .prettierignore

--- src/layouts/Footer/Footer.tsx
+++ src/layouts/Footer/Footer.tsx
@@ -1,14 +1,11 @@
 import type React from "react";
-import type { CSSProperties } from "react";
+import { CSSProperties, useState } from "react";
 
 export default function Footer() {
-  const footerStyle: CSSProperties = {
-    height: "8em",
-    position: "fixed",
-    right: "0",
-    left: "0",
-    bottom: "0",
-  };
+  let [state, setState] = useState(false)
 
-  return <footer style={footerStyle}>Footer</footer>;
+  return <footer
+    className="h-24 max-h-24 fixed left-0 right-0 bottom-0 bg-no-repeat bg-cover"
+    style={{ backgroundImage: "url(/assets/img/footer_bg.png)" }}
+  >Footer</footer>;
 }
