6b98fd6db90551c1b3e42ec59791b30d96940322
Yue-plus <Yue_plus@foxmail.com>
2023-01-30 06:28:58 +0800

    update components Card

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,17 @@
+const colors = require('tailwindcss/colors')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {},
+    colors: {
+      black: colors.black,
+      gray: "#1c1c1c",
+      white: colors.white,
+      arknights: "#22bbff",
+      endfield: "#fdfd1f",
+    }
   },
   plugins: [],
 };
